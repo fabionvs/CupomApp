95b548a7f2068f3de5d09c9dc33a903996d29d63
fabio.garcia <fabio.garcia@dpu.def.br>
2022-05-06 11:01:18 -0300

    Melhorias de bugs

--- services/Api.ts
+++ services/Api.ts
@@ -1,18 +1,22 @@
 /* eslint-disable import/first */
 import axios from "axios";
-import auth from "./Auth";
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const configValue: string = (process.env.REACT_APP_API_URL as string);
 const apiService = axios.create({
     baseURL: "https://api.zenyv.com/api",
 });
+
+const getToken = async () => {
+    let user = await AsyncStorage.getItem('user');
+    return user;
+};
 
 apiService.interceptors.request.use(async config => {
     config.headers = {
         Accept: 'application/json',
         ContentType: 'application/json',
     };
-    const token: any = await auth.getToken();
+    const token: any = await getToken();
     if (token !== null) {
         config.headers = {
             Accept: 'application/json',
